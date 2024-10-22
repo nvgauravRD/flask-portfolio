e688523acb1ec7556fac2e24f24255287b55163a
gaurav nv <gauravvenkat2k22@icloud.com>
2024-10-22 20:50:22 +0530

    Bug fix

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
 
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(port=os.environ.get("PORT", 8000))
+    app.run(port=os.environ.get("PORT", 8000),host="0.0.0.0")
     
